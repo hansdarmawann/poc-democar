034cdc44be2928f91874505c8a7b98fd7a7cae6f
Hans Darmawan <work.hansdarmawan001@gmail.com>
2025-02-09 15:36:39 +0700

    Add Fitur Hapus; Remove While True di Menu;

--- hansdarmawann-jcds2602-capstone-1.py
+++ hansdarmawann-jcds2602-capstone-1.py
@@ -33,6 +33,7 @@ mobil = [
 ]
 
 def lihatMobil(plat):
+    plat = plat.upper().replace(" ","")
     cari = False
     if plat == "":
         data = mobil
@@ -44,31 +45,62 @@ def lihatMobil(plat):
             data = [row for row in mobil if row["platNo"] == plat]
         else:
             print("Data tidak ditemukan.")
-            return
-    print(tabulate(data,headers="keys", maxcolwidths=[11 for i in range(12)]))
+            return 
+            # Kalau gak ada return, maka Terjadi kesalahan saat mencetak data: cannot access local variable 'data' where it is not associated with a value.
+            # Kenapa ? Karena data kosong, sehingga tabulate bingung mau apa.
+    try:
+        print(tabulate(data, headers="keys", maxcolwidths=[12 for i in range(12)]))
+    except Exception as e:
+        print("Terjadi kesalahan saat mencetak data:", e)
+
+
+def hapusMobil(plat):
+    plat = plat.upper().replace(" ","")
+    cari = False
+    for item in mobil:
+        if item["platNo"] == plat and item["ketersediaan"] == True:
+            cari = True
+            mobil.remove(item)
+            print("Data terhapus.")
+            break
+        elif item["platNo"] == plat and item["ketersediaan"] == False:
+            cari = True
+            print("Mobil harus dikembalikan dulu, baru bisa dihapus.")
+            break
+
+    if not cari:
+        print("Data tidak ditemukan.")
+        
 
 def main():
-    while True:
-        try:
-            print("""
-            Selamat Datang di Sistem Peminjaman Mobil Demo XYZ
-            Silahkan Pilih Menu yang Tersedia di Bawah  :
-                1. Lihat Data
-                2. Tambah Mobil
-                3. Update Mobil
-                4. Hapus Mobil
-                5. Keluar
-            """)
-            menu = int(input("Masukkan input anda: "))
-            if menu == 1:
-                plat = input("Masukkan plat nomor anda (kosongkan jika ingin lihat semua data): ").upper().replace(" ", "")
-                lihatMobil(plat)
-            elif menu == 5:
-                print("Terima kasih, sampai jumpa !")
-                break
+    try:
+        print("""
+        Selamat Datang di Sistem Peminjaman Mobil Demo XYZ
+        Silahkan Pilih Menu yang Tersedia di Bawah  :
+            1. Lihat Data
+            2. Tambah Mobil
+            3. Update Mobil
+            4. Hapus Mobil
+            5. Keluar
+        """)
+        menu = int(input("Masukkan input anda: "))
+        if menu == 1:
+            plat = input("Masukkan plat nomor anda (kosongkan jika ingin lihat semua data): ")
+            lihatMobil(plat)
+            main()
+        if menu == 4:
+            lihatMobil("")
+            print()
+            plat = input("Masukkan plat nomor anda: ")
+            hapusMobil(plat)
+            main()
+        elif menu == 5:
+            print("Terima kasih, sampai jumpa !")
+            exit()
 
-        except ValueError:
-            print("Input tidak valid. Silahkan input ulang.")
+    except Exception as e:
+        print("Input tidak valid. Silahkan input ulang.")
+        # print(e) # Uncomment jika ingin trace error.
 
 if __name__ == "__main__":
     main()
